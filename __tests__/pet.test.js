c26efdc7228f2b2ced25281aea22df9e9f6e0e16
James Bowers <jamesbowers123@gmail.com>
2022-03-04 15:45:48 +0000

    add walk function & refactor

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,6 +12,14 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toEqual(0);
     });
+    it('has an initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+    it('has an initial fitness of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
 });
 describe('growUp', () => {
     it('increments the age by 1', () => {
@@ -21,4 +29,44 @@ describe('growUp', () => {
 
         expect(pet.age).toEqual(1);
     });
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+    it('decrements the fitness by 3', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    });
 });
+describe('walk', () => {
+    it('increases fitness by 4', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 4;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(8);
+    });
+    it('increases fitness to a maximum of 10', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 8;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(10);
+    });
+});
+// describe('feed', () => {
+//     it("decreases the pet's hunger by 3", () => {
+//         const pet = new Pet('Fido');
+//         pet.fed();
+
+//         expect(pet.hunger).toBe(3)
+//     })
+// })
--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,29 @@
 
+const APETITE = 5
+
+const MAXIMUM_FITNESS = 10;
+const WALK_EXERCISE = 4
 
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += APETITE;
+    this.fitness -= 3;
 };
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + WALK_EXERCISE) <= MAXIMUM_FITNESS) {
+        this.fitness += WALK_EXERCISE;     
+    } else {
+        this.fitness = MAXIMUM_FITNESS;
+    }
+    
+}
 
 module.exports = Pet;
